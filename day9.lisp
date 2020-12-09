507c616285f37721154124e952bdc7594448bc7a
Paul Jewell <paul@teulu.org>
2020-12-09 16:17:00 +0000

    Day 9 preparation

---
+++ day9.lisp
@@ -0,0 +1,10 @@
+(in-package :advent2020)
+
+(defun day9/test1 ()
+  127) ;; need to change - this is just for debugging the testing issue
+
+(defun day9/solution1 ()
+  )
+
+(defun day9/solution2 ()
+  )
