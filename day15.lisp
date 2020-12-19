4e15865a08710c7f792efba9fd7ddc4ff90aac35
Paul Jewell <paul@teulu.org>
2020-12-19 17:30:29 +0000

    day15 Part2 - but not working in reasonable time...

--- day15.lisp
+++ day15.lisp
@@ -6,12 +6,12 @@
 (defun save-turn (numbers value turn)
   (setf (gethash value numbers) (cons turn (gethash value numbers))))
 
-(defun next-turn (numbers last-number turn)
+(defun next-turn (numbers last-number)
   (let ((turns (gethash last-number numbers)))
     (if (< (length turns) 2)
         0
-        (- (first (gethash last-number numbers))
-           (second (gethash last-number numbers))))))
+        (- (first turns)
+           (second turns)))))
 
 (defun compute-number (v end)
   (let ((numbers (make-hash-table))
@@ -22,15 +22,22 @@
           :do (setf (gethash n numbers) (cons turn (gethash n numbers))))
     (setq last-number (car (last v)))
     ;; Calculate following numbers
-    (loop :for turn :from (1+ (length v)) to end
+    (loop :for turn :from (1+ (length v)) :to end
           :do (progn
-                (setq last-number (next-turn numbers last-number turn))
+                (setq last-number (next-turn numbers last-number))
                 (save-turn numbers last-number turn))
           :finally
              (return last-number))))
 
 (defun test1 ()
-  (compute-number '(0 3 6) 2020))
+  (compute-number test-input 2020))
 
 (defun solution1 ()
   (compute-number input 2020))
+
+(defun test2 ()
+  (compute-number test-input 30000000))
+
+(defun solution2 ()
+  (compute-number input 30000000))
+
