e1eea8ee85fed70165311ac3cb3b3c6acde2bc31
Paul Jewell <paul@teulu.org>
2020-12-28 12:23:02 +0000

    Fixed package issues in the test package
    :use causing name conflicts

--- tests/main.lisp
+++ tests/main.lisp
@@ -6,22 +6,6 @@
 
 (defpackage #:advent2020/test
   (:use #:cl
-        #:day1
-        #:day2
-        #:day3
-        #:day4
-        #:day5
-        #:day6
-        #:day7
-        #:day8
-        #:day9
-        #:day10
-        #:day11
-        #:day12
-        #:day13
-        #:day14
-        #:day15
-        #:day16
         #:fiveam)
   (:export #:run!
            #:all-tests))
